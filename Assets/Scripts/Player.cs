c7d604146a5eb2cbf2aa0cfd8c88dd10ae3603fa
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-12-09 16:49:08 +0700

    GameManager and InputController

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    InputController inputController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputController = GameManager.Instance.InputController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Horizontal " + inputController.Horizontal);
+        print("Mouse " + inputController.MouseInput);
+    }
+}
