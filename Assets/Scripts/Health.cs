27f36dc0a984b509eb69f674cc0059b93b17759a
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-12-10 14:49:50 +0700

    Timer and Respawner

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Health : Destructable
 {
+    [SerializeField] float inSeconds;
+
     public override void Die()
     {
         base.Die();
-        print("We die");
+        GameManager.Instance.Respawner.Despawn(gameObject, inSeconds);
+    }
+
+    void OnEnable()
+    {
+        Reset();
     }
 
     public override void TakeDamage(float amount)
--- Assets/Shared/GameManager.cs
+++ Assets/Shared/GameManager.cs
@@ -17,6 +17,8 @@ public class GameManager
                 m_Instance = new GameManager();
                 m_Instance.gameObject = new GameObject("_gameManager");
                 m_Instance.gameObject.AddComponent<InputController>();
+                m_Instance.gameObject.AddComponent<Timer>();
+                m_Instance.gameObject.AddComponent<Respawner>();
             }
             return m_Instance;
         }
@@ -32,6 +34,32 @@ public class GameManager
                 m_InputController = gameObject.GetComponent<InputController>();
             }
             return m_InputController;
+        }
+    }
+
+    private Timer m_Timer;
+    public Timer Timer
+    {
+        get
+        {
+            if (m_Timer == null)
+            {
+                m_Timer = gameObject.GetComponent<Timer>();
+            }
+            return m_Timer;
+        }
+    }
+
+    private Respawner m_Respawner;
+    public Respawner Respawner
+    {
+        get
+        {
+            if (m_Respawner == null)
+            {
+                m_Respawner = gameObject.GetComponent<Respawner>();
+            }
+            return m_Respawner;
         }
     }
 
