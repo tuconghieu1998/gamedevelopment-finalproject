ed5eaaa85bcc6163afedbe4eff1c2e6e95a8eabe
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-12-11 00:18:43 +0700

    Add mixamo model and animation; Animation Running

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", GameManager.Instance.InputController.Vertical);
+        animator.SetFloat("Horizontal", GameManager.Instance.InputController.Horizontal);
+    }
+}
