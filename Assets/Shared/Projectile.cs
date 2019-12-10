affa813d38c784bcbed8fd77109a73361d9f84c4
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-12-10 12:37:18 +0700

    Shooter

---
+++ Assets/Shared/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float timeToLive;
+    [SerializeField] float damage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToLive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Hit " + other.name);
+    }
+}
