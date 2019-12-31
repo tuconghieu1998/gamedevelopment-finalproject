98f07c19054d7cff537fd1a6235d1ae073539acf
tuconghieu1998 <tuconghieu1998@gmail.com>
2019-12-31 22:42:53 +0700

    create spawnpoint and ragdoll

---
+++ Assets/Ragdoll_Test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll_Test : Destructable
+{
+    [SerializeField] SpawnPoint[] spawnPoints;
+    public Animator animator;
+    private Rigidbody[] bodyParts;
+    private MoveController moveController;
+
+    void Start()
+    {
+        bodyParts = transform.GetComponentsInChildren<Rigidbody>();
+        EnableRagdoll(false);
+        moveController = GetComponent<MoveController>();
+        //print(moveController);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        EnableRagdoll(true);
+        animator.enabled = false;
+
+        GameManager.Instance.Timer.Add(() =>
+        {
+            EnableRagdoll(false);
+            SpawnAtNewSpawnPoint();
+            animator.enabled = true;
+            Reset();
+        }, 5);
+    }
+
+    void SpawnAtNewSpawnPoint()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        transform.position = spawnPoints[spawnIndex].transform.position;
+        transform.rotation = spawnPoints[spawnIndex].transform.rotation;
+    }
+
+    void Update()
+    {
+        if (!isAlive) 
+            return;
+        animator.SetFloat("Vertical", 1);
+        //print(moveController);
+        moveController.Move(new Vector2(5, 0));
+    }
+
+    void EnableRagdoll(bool value)
+    {
+        for(int i = 0; i < bodyParts.Length; i++)
+        {
+            bodyParts[i].isKinematic = !value;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Destructable
+{
+    [SerializeField] SpawnPoint[] spawnPoints;
+
+    void SpawnAtNewSpawnPoint()
+    {
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+        transform.position = spawnPoints[spawnIndex].transform.position;
+        transform.rotation = spawnPoints[spawnIndex].transform.rotation;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+        SpawnAtNewSpawnPoint();
+    }
+
+    [ContextMenu("Test Die")]
+    void TestDie()
+    {
+        Die();
+    }
+}
